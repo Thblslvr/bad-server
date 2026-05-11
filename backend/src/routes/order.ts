2c356ce7fdc46a74923dc404bbccb8bafd3bfe37
Thblslvr <tolebasrudenko@yandex.ru>
2026-05-11 22:57:56 +0500

    fix: revert roleGuard, update frontend lock

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -8,28 +8,17 @@ import {
     getOrdersCurrentUser,
     updateOrder,
 } from '../controllers/order'
-import auth, { roleGuardMiddleware } from '../middlewares/auth'
+import auth from '../middlewares/auth'
 import { validateOrderBody } from '../middlewares/validations'
-import { Role } from '../models/user'
 
 const orderRouter = Router()
 
 orderRouter.post('/', auth, validateOrderBody, createOrder)
-orderRouter.get('/all', auth, roleGuardMiddleware(Role.Admin), getOrders)
+orderRouter.get('/all', auth, getOrders)                     // ← убрали roleGuard
 orderRouter.get('/all/me', auth, getOrdersCurrentUser)
-orderRouter.get(
-    '/:orderNumber',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    getOrderByNumber
-)
+orderRouter.get('/:orderNumber', auth, getOrderByNumber)     // ← убрали roleGuard
 orderRouter.get('/me/:orderNumber', auth, getOrderCurrentUserByNumber)
-orderRouter.patch(
-    '/:orderNumber',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    updateOrder
-)
-orderRouter.delete('/:id', auth, roleGuardMiddleware(Role.Admin), deleteOrder)
+orderRouter.patch('/:orderNumber', auth, updateOrder)        // ← убрали roleGuard
+orderRouter.delete('/:id', auth, deleteOrder)                // ← убрали roleGuard
 
 export default orderRouter
