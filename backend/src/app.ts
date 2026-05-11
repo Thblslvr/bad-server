c0c846ac89d05fb971196a4ab3cf9226d59576c3
Thblslvr <tolebasrudenko@yandex.ru>
2026-05-11 20:29:49 +0500

    debug: временно убрать helmet и rateLimit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,6 @@ import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
-import helmet from 'helmet'
-import rateLimit from 'express-rate-limit'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import routes from './routes'
@@ -14,22 +12,8 @@ import routes from './routes'
 const { PORT = 3000 } = process.env
 const app = express()
 
-// Заголовки безопасности
-app.use(helmet())
-
-// Ограничение частоты запросов
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-})
-app.use(limiter)
-
 app.use(cookieParser())
 app.use(cors())
-
-// Безопасная раздача статики
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
